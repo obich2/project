71ec16d1cfa5d10dd813abaaf2fe086a82d1afbb
obich <g.shchet@gmail.com>
2024-02-22 19:31:03 +0530

    start window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame as pg
 from settings import *
+import pygame.display
+import time
+import sys
 
 
 def get_font(size):
@@ -20,5 +23,43 @@ class TextLine:
         self.rect = self.text.get_rect(center=self.position)
         sc.blit(self.text, self.rect)
 
+
+def main_menu():
+    while True:
+
+        sc.fill('black')
+        first = TextLine(sc, 100, 'MOVE', (800, 450), 'WHITE')
+        fourth = TextLine(sc, 20, 'press any button to START', (800, 800), 'GREY')
+        fourth.draw()
+        first.draw()
+        pg.display.update()
+
+        time.sleep(2)
+
+        sc.fill('black')
+        second = TextLine(sc, 100, 'OR', (800, 450), 'WHITE')
+        second.draw()
+        fifth = TextLine(sc, 20, 'press any button to START', (800, 800), 'GREY')
+        fifth.draw()
+        pg.display.update()
+
+        time.sleep(2)
+
+        sc.fill('black')
+        third = TextLine(sc, 100, 'DIEEE!!!!!', (800, 450), 'RED')
+        third.draw()
+        six = TextLine(sc, 20, 'press any button to START', (800, 800), 'GREY')
+        six.draw()
+        pg.display.update()
+
+        time.sleep(2)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+
 pg.init()
 sc = pg.display.set_mode((WIDTH, HEIGHT))
+main_menu()
